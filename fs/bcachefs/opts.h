918083a3bea2f3a6d3d64181220693adb050041c
Kent Overstreet <kent.overstreet@linux.dev>
2026-04-19 18:48:04 -0400

    Revert "bcachefs: opts.btree_cache_size_max"
    This reverts commit 74fd63f4cc4fd24f088a4838312fd5b8ec87ab24.

--- fs/bcachefs/opts.h
+++ fs/bcachefs/opts.h
@@ -251,11 +251,6 @@ enum fsck_err_opts {
 	  OPT_BOOL(),							\
 	  BCH2_NO_SB_OPT,		true,				\
 	  NULL,		"Stash pointer to in memory btree node in btree ptr")\
-	x(btree_cache_size_max,		u64,				\
-	  OPT_FS|OPT_MOUNT|OPT_RUNTIME|OPT_HUMAN_READABLE,		\
-	  OPT_UINT(0, U64_MAX),						\
-	  BCH2_NO_SB_OPT,		0,				\
-	  NULL,		"Maximum size of the btree node cache in bytes (0 = no limit)")\
 	x(gc_reserve_percent,		u8,				\
 	  OPT_FS|OPT_FORMAT|OPT_MOUNT|OPT_RUNTIME,			\
 	  OPT_UINT(5, 21),						\
